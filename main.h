6429494e74706c1de3aa11d3e773ec713eaf94de
Vin Samuel <vinscool@live.ca>
2022-03-06 09:16:22 -0500

    initial commit, it works ok
    there is missing stuff, but it's work in progress

---
+++ main.h
@@ -0,0 +1,24 @@
+﻿// POKEY Frequencies Calculator
+// Most of the global stuff will be defined here
+
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <fstream>
+#include <time.h>
+#include <cmath>
+
+#define VERSION "v0.1"
+#define FREQ_17_NTSC 1789773 
+#define FREQ_17_PAL 1773447
+#define SEPARATOR cout << endl << "----------------------------------------------------" << endl
+
+int main();
+void wait(int sec);
+void real_freq();
+void generate_freq();
+void generate_table(int note, double freq, int distortion, bool CLOCK_15, bool CLOCK_179, bool JOIN_16BIT);
+
+
